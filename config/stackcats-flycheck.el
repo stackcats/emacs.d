467ca8d36482a43e78ebf5ff6680e62e7d518dfc
stackcats <646035537@qq.com>
2015-11-14 16:29:33 +0800

    flycheck-bitmap

--- config/stackcats-flycheck.el
+++ config/stackcats-flycheck.el
@@ -1,6 +1,41 @@
 ;;; Code:
-(require 'diminish)
+
+(when (fboundp 'define-fringe-bitmap)
+  (define-fringe-bitmap 'my-flycheck-fringe-indicator
+    (vector #b00000000
+	    #b00000000
+	    #b00000000
+	    #b00000000
+	    #b00000000
+	    #b00000000
+	    #b00000000
+	    #b00011100
+	    #b00111110
+	    #b00111110
+	    #b00111110
+	    #b00011100
+	    #b00000000
+	    #b00000000
+	    #b00000000
+	    #b00000000
+	    #b01111111)))
+
+(flycheck-define-error-level 'error
+  :overlay-category 'flycheck-error-overlay
+  :fringe-bitmap 'my-flycheck-fringe-indicator
+  :fringe-face 'flycheck-fringe-error)
+
+(flycheck-define-error-level 'warning
+  :overlay-category 'flycheck-warning-overlay
+  :fringe-bitmap 'my-flycheck-fringe-indicator
+  :fringe-face 'flycheck-fringe-warning)
+
+(flycheck-define-error-level 'info
+  :overlay-category 'flycheck-info-overlay
+  :fringe-bitmap 'my-flycheck-fringe-indicator
+  :fringe-face 'flycheck-fringe-info)
 (add-hook 'after-init-hook #'global-flycheck-mode)
+
 (with-eval-after-load 'flycheck
   (flycheck-pos-tip-mode)
   (diminish 'flycheck-mode "ⓕ")
