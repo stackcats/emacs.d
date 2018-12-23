171cdd9a1210310bda4db21f821d85bcf2d97127
stackcats <stackcats@foxmail.com>
2018-12-24 00:10:55 +0800

    make web-mode play nice with smartparens

--- config/stackcats-parens.el
+++ config/stackcats-parens.el
@@ -6,6 +6,16 @@
   :config
   (setq-default sp-escape-quotes-after-insert nil)
   (require 'smartparens-config)
+  (sp-with-modes '(web-mode)
+    (sp-local-pair "%" "%"
+                   :unless '(sp-in-string-p)
+                   :post-handlers '(((lambda (&rest _ignored)
+                                       (just-one-space)
+                                       (save-excursion (insert " ")))
+                                     "SPC" "=" "#")))
+    (sp-local-tag "%" "<% "  " %>")
+    (sp-local-tag "=" "<%= " " %>")
+    (sp-local-tag "#" "<%# " " %>"))
   (add-hook 'css-mode-hook 'smartparens-mode))
 
 (provide 'stackcats-parens)
--- config/stackcats-web.el
+++ config/stackcats-web.el
@@ -3,7 +3,9 @@
 ;;; Code:
 (use-package web-mode
   :mode (("\\.html\\'" . web-mode)
-         ("\\.html\\.eex\\'" . web-mode)))
+         ("\\.html\\.eex\\'" . web-mode))
+  :config
+  (setq web-mode-enable-auto-pairing nil))
 
 (provide 'stackcats-web)
 ;;; stackcats-web.el ends here
