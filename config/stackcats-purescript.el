83ddaaec0a18dc794273afc806e3b4307ce9d7f8
stackcats <stackcats@foxmail.com>
2021-11-21 20:33:10 +0800

    update purescript config

--- config/stackcats-purescript.el
+++ config/stackcats-purescript.el
@@ -2,18 +2,20 @@
 ;;; Commentary:
 ;;; Code:
 (use-package purescript-mode
-  :ensure-system-package
-  ((purs . "npm install -g purescript")
-   (pulp . "npm install -g pulp bower"))
-  :mode "\\.purs$")
-
-
-(use-package psc-ide
-  :hook
-  ((purescript-mode . psc-ide-mode)
-   (purescript-mode . turn-on-purescript-indentation))
+  :mode "\\.purs\\'"
   :config
-  (setq psc-ide-host "127.0.0.1"))
+  (use-package psc-ide
+    :bind
+    (:map psc-ide-mode-map
+          ("TAB" . company-complete))
+    :config
+    (setq psc-ide-use-npm-bin t)
+    (add-hook 'purescript-mode-hook
+              (lambda ()
+                (psc-ide-mode)
+                (company-mode)
+                (flycheck-mode)
+                (turn-on-purescript-indentation)))))
 
 (provide 'stackcats-purescript)
 ;;; stackcats-purescript.el ends here
