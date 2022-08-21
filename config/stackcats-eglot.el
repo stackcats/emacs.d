3811b9a968342ac8a2a43335b47a097f1807286e
stackcats <stackcats@foxmail.com>
2022-08-21 21:09:15 +0800

    update elixir-ls

--- config/stackcats-eglot.el
+++ config/stackcats-eglot.el
@@ -5,7 +5,7 @@
   :config
   (add-to-list 'eglot-server-programs '((c++-mode c-mode) "clangd"))
   (add-to-list 'eglot-server-programs '(rust-mode . "rust-analyzer"))
-  (add-to-list 'eglot-server-programs '(elixir-mode "~/.emacs.d/vendor/elixir-ls-1.12/language_server.sh")))
+  (add-to-list 'eglot-server-programs '(elixir-mode "~/.emacs.d/vendor/elixir-ls-1.13-24.3/language_server.sh")))
 
 (provide 'stackcats-eglot)
 ;;; stackcats-eglot.el ends here
