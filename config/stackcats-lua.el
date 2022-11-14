bc0b2e23de131f95bdc5168deedb7be68fcd8e03
stackcats <stackcats.hk@gmail.com>
2022-11-14 17:11:17 +0800

    add lua-mode

--- config/stackcats-lua.el
+++ config/stackcats-lua.el
@@ -1,17 +1,16 @@
 ;;; package --- Summary
 ;;; Commentary:
 ;;; Code:
-;; (use-package lua-mode
-;;   :mode "\\.lua\\'"
-;;   :interpreter "lua"
-;;   :bind
-;;   (:map lua-mode-map
-;;   ("C-c c" . lua-send-buffer)))
+(use-package lua-mode
+  :mode "\\.lua\\'"
+  :config
+  (setq lua-indent-level 4)
+  (setq lua-indent-nested-block-content-align nil))
 
-;; (use-package company-lua
-;;   :after (company)
-;;   :config
-;;   (add-to-list 'company-backends 'company-lua))
+(use-package company-lua
+  :after (company)
+  :config
+  (add-to-list 'company-backends 'company-lua))
 
 (provide 'stackcats-lua)
 ;;; stackcats-lua.el ends here
