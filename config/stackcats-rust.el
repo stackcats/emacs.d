34cddc14ff753ac0b8a460f19885e04d53d1ef1e
stackcats <stackcats.hk@gmail.com>
2023-01-21 23:21:40 +0800

    use flycheck & rust clippy

--- config/stackcats-rust.el
+++ config/stackcats-rust.el
@@ -7,6 +7,7 @@
   :config
   (setq rustic-lsp-client 'eglot)
   (add-hook 'eglot-managed-mode-hook (lambda () (flymake-mode -1)))
+  (push 'rustic-clippy flycheck-checkers)
   (setq rustic-format-trigger 'on-save))
 
 (provide 'stackcats-rust)
