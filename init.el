231e2eecdd6bfc9bf71a051b816b7499a940bf6e
stackcats <stackcats.hk@gmail.com>
2023-07-26 16:14:57 +0800

    add tree-sitter config

--- init.el
+++ init.el
@@ -414,6 +414,14 @@
   :config
   (setcdr (assoc "Python" format-all-default-formatters) '(yapf)))
 
+(use-package tree-sitter-langs)
+
+(use-package tree-sitter
+  :after tree-sitter-langs
+  :config
+  (global-tree-sitter-mode)
+  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
+
 (defun stackcats/c-mode-setup ()
   (c-toggle-comment-style -1)
   (setq indent-tabs-mode t))
